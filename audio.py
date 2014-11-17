8885317851a208c982d1576fec0d87ec41d97227
Tyler Lubeck <tyler@tylerlubeck.com>
2014-11-17 16:20:01 -0500

    Working pyaudo setup

--- audio.py
+++ audio.py
@@ -1,7 +1,6 @@
-#Audio.py
-#defines play
+# Audio.py
+# defines play
 
-import sys
 
 class Audio():
 
@@ -10,36 +9,39 @@ class Audio():
 
     def get_play(self):
         import math
-        import struct
-        amplitude = 0.5
-        fs = 48000
+        # amplitude = 0.5
+        bit_rate = 16000
         try:
             # use pyaudio for osx
             import pyaudio
+
             def real_play(freq, duration):
                 p = pyaudio.PyAudio()
-                # todo: maybe don't do every time??????????????????????????????????????
-                stream = p.open(format=pyaudio.paFloat32, channels=1,
-                                rate=fs, output=True)
-                N = int(fs / freq)
-                T = int(freq * duration)  # repeat for T cycles
-                dt = 1.0 / fs
-                # 1 cycle
-                tone = (amplitude * math.sin(2 * math.pi * freq * n * dt)
-                        for n in xrange(N))
-                # todo: get the format from the stream; this assumes Float32
-                data = ''.join(struct.pack('f', samp) for samp in tone)
-                for n in xrange(T):
-                    stream.write(data)
-                stream.close()
-                print('Play!')
+                # TODO: maybe don't do every time?
+                stream = p.open(format=p.get_format_from_width(1),
+                                channels=1,
+                                rate=bit_rate,
+                                output=True)
+                number_of_frames = int(bit_rate * duration)
+                rest_frames = number_of_frames % bit_rate
+                wavedata = ''
+                for x in xrange(number_of_frames):
+                    wavedata += chr(int(math.sin(x/((bit_rate/freq)/math.pi))*127+128))
 
+                for x in xrange(rest_frames):
+                    wavedata += chr(128)
+
+                stream.write(wavedata)
+                stream.stop_stream()
+                stream.close()
+                p.terminate()
             return real_play
 
         except ImportError, e1:
             try:
                 # use winsound for windows
                 import winsound
+
                 def real_play(freq, duration):
                     raise e1
                     winsound.Beep(freq, duration)
@@ -57,4 +59,8 @@ class Audio():
 
 if __name__ == '__main__':
     a = Audio()
-    a.play(10, 10)
+    a.play(261.63, 1.2232)
+    a.play(277.2, 1.2232)
+    a.play(293.7, 1.2232)
+    a.play(311.1, 1.2232)
+    a.play(329.6, 1.2232)
