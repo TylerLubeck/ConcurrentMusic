60ccf23f2537fa99affffed5244884277b674d99
acompt <andreacompton54@gmail.com>
2014-11-22 13:05:45 -0500

    added note_to_freq

---
+++ note_to_freq.py
@@ -0,0 +1,27 @@
+
+FREQUENCIES = {'A': 440.0, 'B': 493.9, 'C': 261.6, 'D': 293.7, 
+               'E': 329.6, 'F': 349.2, 'G': 392.0};
+
+def get_freq(note):
+	return FREQUENCIES[note]
+
+def get_duration(bpm, length, hold):
+	#pass in number of holds?
+	duration = 1/length
+	if hold != 0: duration = duration * (hold + 1)
+
+	return (60 / bpm) * duration
+
+#C         261.6
+#C#        277.2
+#D         293.7
+#D#        311.1
+#E         329.6
+#F         349.2
+#F#        370.0
+#G         392.0
+#G#        415.3
+#A         440.0
+#A#        466.2
+#B         493.9
+#C         523.2
